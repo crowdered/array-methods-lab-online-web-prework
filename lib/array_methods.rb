e1bd1a3f62933374bdfc394b425552d1b4f22666
Mark Crowder <crowdered@gmail.com>
2019-03-03 18:30:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,5 @@
 def using_include(array, element)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
-element = ("wow")
-array.include?(element)
-end
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
-element = ("Tardis")
-array.include?(element)
-end
+
 
 def using_sort(array)
 
